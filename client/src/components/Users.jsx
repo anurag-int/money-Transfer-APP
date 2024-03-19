2baca28cb99ad71d32f9494bd7bb11047156de41
anurag-int <ag6526803@gmail.com>
2024-03-19 06:44:13 +0530

    Dashboard component update

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,24 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
+
 export function Users(){
+    const [users, setUsers] = useState([]);
+    const [id, setId] = useState("");
+    const [userName, setUserName] = useState("");
+    const history = useHistory();
+
+    useEffect(()=>{
+      axios.get(`http://localhost:3000/api/v1/user/bulk`)
+      .then(response => {
+        console.log(response.data.users)
+        setUsers(response.data.users );
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    }, [users])
+
     return <div>
-        <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
-                  <div className="rounded-full w-6 text-center hover:border-black bg-gray-300 ml-2">A</div>
-                  <p className="text-white pr-2 hover:text-black ">Anurag Gupta</p>
-                </button>
-                <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
-                  <div className="rounded-full w-6 text-center bg-gray-300 ml-2">A</div>
-                  <p className="text-white pr-2">Vineet Singh</p>
-                </button>
-                <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
-                  <div className="rounded-full w-6 text-center bg-gray-300 ml-2">A</div>
-                  <p className="text-white pr-2">Sourabh Singh</p>
-                </button>
-                <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
-                  <div className="rounded-full w-6 text-center bg-gray-300 ml-2">A</div>
-                  <p className="text-white pr-2">Swapnil Singh</p>
-                </button>
-                <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
-                  <div className="rounded-full w-6 text-center bg-gray-300 ml-2">A</div>
-                  <p className="text-white pr-2">Om Rathore</p>
-                </button>
+
+              {
+                users.map((user) => (
+                  <button className="flex flex-row space-x-3 hover:bg-gray-400 rounded-2xl p-1 cursor-pointer">
+                  
+                    <div className="rounded-full hover:bfull w-6 text-order-black bg-gray-300 ml-2">A</div>
+                    <p className="text-white pr-2 hover:text-black ">{user.firstName} {user.lastName}</p>
+                  </button>
+                ))
+              }
+              
     </div>
 }
+
