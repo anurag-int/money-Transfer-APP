c4689140b82d8998b3601f35f747b9b965e52aef
anurag-int <ag6526803@gmail.com>
2024-03-20 03:22:10 +0530

    User component update

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -1,6 +1,10 @@
 import { Users } from "./Users"
 import { Balance } from "./Balance";
+import { useState } from "react";
+
 export function Appbar() {
+  const [filter, setFilter] = useState("");
+
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -21,11 +25,7 @@ export function Appbar() {
 
               <div className="space-y-4">
                 <p className="text-white">Your Contacts</p>
-                <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Users..." required/>
-                  </div>
-                  <Users></Users>
+                <Users></Users>
               </div>
             </div>
           </div>
